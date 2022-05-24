4b1f4029dede7302d6d658a4cca073f8afcb9385
Druv Pai <druv@druvpai.com>
2022-05-23 22:45:31 -0700

    typo fix

--- supervised_experiment.py
+++ supervised_experiment.py
@@ -43,7 +43,7 @@ def supervised_experiment(model: pl.LightningModule, data: pl.LightningDataModul
         plot_cosine_similarity_X(X, result_dir)
         plot_cosine_similarity_fX(fX, result_dir)
         plot_cosine_similarity_gfX(gfX, result_dir)
-        plot_cosine_similarity_fgfX(gfX, result_dir)
+        plot_cosine_similarity_fgfX(fgfX, result_dir)
         plot_E_C(model, result_dir)
 
     elif isinstance(model, SupervisedGAN):
