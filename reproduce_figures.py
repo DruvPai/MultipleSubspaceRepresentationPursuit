8552f9a8dda6e1cd259e9008892f155ba75a9942
Druv Pai <druv@druvpai.com>
2022-09-25 10:26:13 -0700

    editing to match new notation

---
+++ reproduce_figures.py
@@ -0,0 +1,32 @@
+from supervised_experiment import *
+
+
+def reproduce_fig_1():
+    # Question: what is baseline performance on correlated data?
+    ctrl_msp_experiment(
+        n=[500, 500, 500], k=3, d_x=50, d_z=40, d_S=[3, 4, 5]
+    )
+
+
+def reproduce_fig_2():
+    # Question: what is performance on correlated data with added noise?
+    ctrl_msp_experiment(
+        n=[500, 500, 500], k=3, d_x=50, d_z=40, d_S=[3, 4, 5],
+        sigma_sq=1e-2
+    )
+
+
+def reproduce_fig_3():
+    # Question: what is default performance on real data?
+    ctrl_msp_mnist_experiment(d_z=150)
+
+
+def reproduce_fig_4():
+    # Question: what is default performance on real data, when tuning with neural networks?
+    ctrl_msp_fcnn_mnist_experiment(d_z=150, d_latent=150, n_layers=3)
+
+
+reproduce_fig_1()
+reproduce_fig_2()
+reproduce_fig_3()
+reproduce_fig_4()
